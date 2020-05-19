1610eff928f317ea48cb1cac4fc1ad1a28875ab8
thecerealcoder <thecerealcoder@gmail.com>
2020-05-19 07:09:13 -0500

    Refined project card design

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -10,20 +10,24 @@ function Body() {
 
     const work = {
         brewguru: {
+            name: "BrewGuru",
             path: "demos/brewguru.mp4",
-            description: "ddd"
+            description: "Aliquip eiusmod enim aliqua ea ad laboris reprehenderit ea dolore fugiat adipisicing velit minim dolore. Lorem id ad ullamco labore ex ipsum mollit non nostrud Lorem adipisicing id quis. Anim incididunt mollit veniam cillum pariatur aliqua non duis ex veniam exercitation eu duis."
         },
         covid: {
+            name: "COVID-19 Tracker",
             path: "demos/covid.mp4",
-            description: "ddd"
+            description: "Aliqua dolore pariatur aliquip quis id eu aliquip. Ex est fugiat magna commodo aliquip non excepteur officia id mollit eu. Dolore ipsum reprehenderit ut dolore minim minim sunt nisi labore eiusmod. Do nulla dolore et esse quis ea adipisicing aute cillum esse. Velit elit duis aliquip do eiusmod labore eiusmod sunt aute proident esse aute exercitation. Consequat veniam minim qui ut mollit culpa id laborum. Incididunt non cupidatat amet esse laboris Lorem dolore occaecat."  
         },
         weather: {
+            name: "WeatherMe",
             path: "demos/weather.mp4",
-            description: "ddd"
+            description: "Consequat ad deserunt cupidatat velit laborum. Anim veniam est eu minim sint duis tempor ex sit pariatur cillum adipisicing dolore irure. Adipisicing aliqua labore amet eu dolore deserunt nulla. Minim proident ad voluptate amet voluptate incididunt culpa laboris ad do magna ad cillum. Quis dolore non minim ad esse ipsum et laborum deserunt velit labore voluptate laborum Lorem."
         },
         thecerealcoder: {
+            name: "TheCerealCoder",
             path: "demos/thecerealcoder.mp4",
-            description: "ddd"
+            description: "Aute mollit nisi eu voluptate. Cillum labore eu enim exercitation ea ipsum esse cillum ad sint minim exercitation. In aliqua quis officia aute aliqua ipsum amet elit cupidatat adipisicing do. Culpa ut culpa id qui amet aute aute. Pariatur id dolore excepteur id non et est cillum mollit est incididunt ipsum excepteur incididunt. Dolor tempor fugiat tempor deserunt aute ea duis sint ut est. Nostrud velit deserunt aliquip reprehenderit occaecat duis id."
         }
     };
 
@@ -39,37 +43,21 @@ function Body() {
                 </p>
             </div>
             <hr />
-            <Carousel infiniteLoop interval={4500} showStatus={false} showThumbs={false} className={styles.work}>
-                <div className={styles.imgContainer}>
-                    <img src="demos/screen.png"></img>
-                    <div className={styles.readMore}>
-                        <h1>COVID Tracker</h1>
-                        <button value="covid" className="open" onClick={ev => handleClick(ev)}>
-                            Read More
-                            </button>
-                    </div>
-                </div>
-                <div className={styles.imgContainer}>
-                    <img src="demos/screen.png"></img>
-                    <div className={styles.readMore}>
-                        <h1>BrewGuru</h1>
-                        <button value="brewguru" className="open" onClick={ev => handleClick(ev)}>
-                            Read More
-                        </button>
-                    </div>
-                </div>
-                <div className={styles.imgContainer}>
-                    <img src="demos/screen.png"></img>
-                    <div className={styles.readMore}>
-                        <h1>Weather</h1>
-                        <button value="weather" className="open" onClick={ev => handleClick(ev)}>
-                            Read More
-                            </button>
-                    </div>
-                </div>
-            </Carousel>
-            <hr />
-            <Overlay project={project}/>
+            <div className={styles.carouselContainer}>
+                <Carousel infiniteLoop interval={4500} showStatus={false} showThumbs={false} className={styles.work}>
+                    <button value="covid" className={`${styles.imgContainer} open`} onClick={ev => handleClick(ev)}>
+                        <img src="demos/screen.png"></img>
+                    </button>
+                    <button value="brewguru" className={`${styles.imgContainer} open`} onClick={ev => handleClick(ev)}>
+                        <img src="demos/screen.png"></img>
+                    </button>
+                    <button value="weather" className={`${styles.imgContainer} open`} onClick={ev => handleClick(ev)}>
+                        <img src="demos/screen.png"></img>
+                    </button>
+                </Carousel>
+                <hr />
+                <Overlay project={project}/>
+            </div>
         </div>
     )
 };
--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Overlay.module.css';
 
-function Overlay({project: {path, description}}) {
+function Overlay({project: {name, path, description}}) {
     return(
         <div id="overlay">
             <div className={styles.projectCard}>
-                <video src={path} autoPlay loop muted></video>
-                <div>
+                <div className={styles.vidContainer}>
+                    <video src={path} autoPlay loop muted></video>
+                </div>
+                <div className={styles.details}>
+                    <h1>{name}</h1>
                     <p>{description}</p>
                 </div>
             </div>
