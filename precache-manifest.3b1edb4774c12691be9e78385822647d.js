40c9588447891917bd76494f50c8ec526bbf4774
thecerealcoder <thecerealcoder@gmail.com>
2020-05-24 20:46:29 -0500

    Updates

--- precache-manifest.3b1edb4774c12691be9e78385822647d.js
+++ precache-manifest.3b1edb4774c12691be9e78385822647d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03d24587fb0f717c2ee576f902896740",
+    "revision": "240baeae323abfca827bbe41daddda7e",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "0fd956a0d255c09c4dc6",
+    "revision": "0cc272d39866aba5f724",
     "url": "/portfolio/static/css/2.ddcb8a2a.chunk.css"
   },
   {
-    "revision": "9c8c56455f386fc8eae3",
+    "revision": "5939bdeaeefdd0da0044",
     "url": "/portfolio/static/css/main.8ea837fd.chunk.css"
   },
   {
-    "revision": "0fd956a0d255c09c4dc6",
-    "url": "/portfolio/static/js/2.e72c33a1.chunk.js"
+    "revision": "0cc272d39866aba5f724",
+    "url": "/portfolio/static/js/2.aaa0b770.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/portfolio/static/js/2.e72c33a1.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.aaa0b770.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c8c56455f386fc8eae3",
-    "url": "/portfolio/static/js/main.71cb3016.chunk.js"
+    "revision": "5939bdeaeefdd0da0044",
+    "url": "/portfolio/static/js/main.c3e4c2d5.chunk.js"
   },
   {
     "revision": "e5284505c87d0b24b753",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.96521b8ddc7905a6f81b846cb736e956.js"
+  "/portfolio/precache-manifest.3b1edb4774c12691be9e78385822647d.js"
 );
 
 self.addEventListener('message', (event) => {
